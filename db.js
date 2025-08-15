a3a9dd51d0700d712b2ceff1d088dbfd3cc6abcb
MM-Mamunne <gamesmamun8@gmail.com>
2025-08-15 12:11:36 +0600

    m7

--- db.js
+++ db.js
@@ -16,7 +16,8 @@ import pg from "pg";
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString:
+    "postgresql://neondb_owner:npg_RNDL5TwtKCo1@ep-raspy-dawn-ad5fix8j-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
   ssl: { rejectUnauthorized: false }, // Needed for Neon
 });
 
