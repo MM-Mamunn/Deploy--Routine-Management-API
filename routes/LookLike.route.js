8f3615fd9c5fa5eb0a4674c9daeda67ea1c90f66
MM-Mamunne <gamesmamun8@gmail.com>
2025-08-16 13:43:38 +0600

    session section added

--- routes/LookLike.route.js
+++ routes/LookLike.route.js
@@ -4,9 +4,11 @@ import {
   courseLookLike,
   sectionLookLike,
   facultyLookLike,
+  sessionLookLike,
 } from "../controllers/LookLike.controller.js";
 router.get("/courseLookLike/:slug", courseLookLike);
 router.get("/sectionLookLike/:slug", sectionLookLike);
 router.get("/facultyLookLike/:slug", facultyLookLike);
+router.get("/sessionLookLike/:slug", sessionLookLike);
 
 export default router;
