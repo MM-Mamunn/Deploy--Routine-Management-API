f555e584349af6999d6d06512bd3384005d8fec7
MM-Mamunne <gamesmamun8@gmail.com>
2025-08-16 21:02:49 +0600

    minor

--- routes/class.route.js
+++ routes/class.route.js
@@ -1,8 +1,10 @@
-import express from "express";
+import express from 'express';
 const router = express.Router();
-import { classEntry } from "../controllers/class.controller.js";
-import crAuthorizan from "../middlewares/crAuthorizan.js";
+import { classEntry } from '../controllers/class.controller.js';
+// import studentAuthorization from '../middlewares/studentAuthorizan.js';
+import crAuthorizan from '../middlewares/crAuthorizan.js';
 
-router.post("/new", crAuthorizan, classEntry);
+router.post('/new',crAuthorizan,classEntry);
+
 
 export default router;
