289deed83759d32061de8fa9b2c9641389b94d8a
MM-Mamunne <gamesmamun8@gmail.com>
2025-08-16 18:44:55 +0600

    spam control data entry

--- controllers/class.controller.js
+++ controllers/class.controller.js
@@ -3,14 +3,26 @@ import pool from "../db.js";
 //authorizeentication
 
 const classEntry = async (req, res) => {
-  //   const { section  } = req.body;
-  // const section = req.params.slug;
   const { session, section, code, faculty, room, day, slot } = req.body;
   try {
+    // Check for existing class
+    const checkExist = await pool.query(
+      `SELECT * FROM class 
+       WHERE session = $1 AND sec = $2 AND code = $3 AND faculty = $4 AND room = $5 AND day = $6 AND slot = $7`,
+      [session, section, code, faculty, room, day, slot]
+    );
+
+    if (checkExist.rows.length > 0) {
+      // Found duplicate row
+      return res
+        .status(400)
+        .json({ error: "Class entry already exists with these details." });
+    }
+
+    // Insert new class if not exists
     const newClass = await pool.query(
-      `insert into class(session, sec,code ,faculty, room,day,slot)
-values
-($1,$2,$3,$4,$5,$6,$7) RETURNING *;`,
+      `INSERT INTO class(session, sec, code, faculty, room, day, slot)
+       VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;`,
       [session, section, code, faculty, room, day, slot]
     );
 
@@ -20,5 +32,4 @@ values
     res.status(500).send("Server error");
   }
 };
-
 export { classEntry };
